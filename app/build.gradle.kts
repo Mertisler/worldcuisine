b56496e8291fe7a7680cf9e5c574c26656ae7301
mertisler <112168951+Mertisler@users.noreply.github.com>
2025-09-28 15:12:55 +0300

    proje için gerekli kütüphaneler eklendi : Retrofit Lifecycle & ViewModel Compose Coroutines Hilt Room components

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -28,12 +30,13 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
+
     buildFeatures {
         compose = true
     }
@@ -56,4 +59,34 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+// Lifecycle & ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
+// Compose (varsa)
+    implementation("androidx.navigation:navigation-compose:2.6.0")
+    implementation("androidx.activity:activity-compose:1.7.2")
+// Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
+// OkHttp (opsiyonel - log için)
+    implementation("com.squareup.okhttp3:logging-interceptor:4.9.1")
+
+    implementation("io.coil-kt:coil-compose:2.2.2") // veya son sürüm
+
+    implementation("com.google.dagger:hilt-android:2.44")
+    kapt("com.google.dagger:hilt-android-compiler:2.44")
+
+    // Room components
+    implementation("androidx.room:room-runtime:2.2.5")
+    kapt("androidx.room:room-compiler:2.2.5")
+    implementation("androidx.room:room-ktx:2.2.5")
+    androidTestImplementation("androidx.room:room-testing:2.2.5")
+
+}
+
+kapt {
+    correctErrorTypes = true
 }
